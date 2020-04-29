195fe52163d543822df2497fc2b9044f78b4b9cb
rami6 <30137645+rami6@users.noreply.github.com>
2020-04-29 00:02:42 -0700

    Make SNS-share buttons work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
-import { tipsData } from './situation/tips-data';
+import {Component} from '@angular/core';
+import {faCaretRight} from '@fortawesome/free-solid-svg-icons';
+import {tipsData} from './situation/tips-data';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +12,9 @@ export class AppComponent {
   faCaretRight = faCaretRight;
 
   situations = tipsData;
+
+  urlToBeShared = 'https://make-2020-better.github.io/kazokuwomamorou/';
+  lineShareLink = `https://social-plugins.line.me/lineit/share?url=${this.urlToBeShared}`;
+  fbShareLink = `https://www.facebook.com/sharer/sharer.php?u=${this.urlToBeShared}`;
+  twitterShareLink = `http://twitter.com/share?text=家族を守ろう − 自分と大切な人を守るために、今できること。&url=${this.urlToBeShared}`;
 }
