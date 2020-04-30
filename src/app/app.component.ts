a6757d943c5b62800dffefccd11e4ebab375ea2c
rami6 <30137645+rami6@users.noreply.github.com>
2020-04-30 00:15:10 -0700

    Set up for using custom domain

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit {
 
   situations = tipsData;
 
-  urlToBeShared = 'https://make-2020-better.github.io/kazokuwomamorou/';
+  urlToBeShared = 'http://家族を守ろう.com';
   lineShareLink = `https://social-plugins.line.me/lineit/share?url=${this.urlToBeShared}`;
   fbShareLink = `https://www.facebook.com/sharer/sharer.php?u=${this.urlToBeShared}`;
   twitterShareLink = `http://twitter.com/share?text=家族を守ろう − 自分と大切な人を守るために、今できること。&url=${this.urlToBeShared}`;
@@ -32,7 +32,7 @@ export class AppComponent implements OnInit {
     this.metaService.addTags([
       {name: 'og:site_name', content: '家族を守ろう'},
       {name: 'og:type', content: 'website'},
-      {name: 'og:url', content: 'https://make-2020-better.github.io/kazokuwomamorou/'},
+      {name: 'og:url', content: 'http://家族を守ろう.com'},
       {name: 'og:title', content: '家族を守ろう'},
       {name: 'og:description', content: '今を安全に過ごし、日常を早く取り戻すためにできることを状況別にまとめました。'},
       {name: 'og:image', content: 'ogp.png'},
